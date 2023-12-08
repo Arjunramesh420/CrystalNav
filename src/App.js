4d9ce110d495cd79361b7c157fe9f4a8596ac6dc
ArjunRamesh <arjunramesh2001@gmail.com>
2023-12-08 13:59:18 +0530

    Transparent Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <div className="App">
+<nav class="navbar navbar-expand-sm navbar-dark bg fixed-top transparent">
+<a href="#home" className="navbar-brand "><img  className="img-fluid " style={{marginLeft:'8px'}} src={logogig} width="35px" alt=""/></a>
+    <button className="navbar-toggler" type="button"  data-bs-toggle="collapse"  data-bs-target="#navmenu">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" style={{marginLeft:'8px'}} id="navmenu">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item ">
+          <a className="nav-link" href="#home">Home</a>
+        </li> 
+        <li className="nav-item ">
+          <a className="nav-link" href="#about">About Us</a>
+        </li>
+        <li className="nav-item ">
+          <a className="nav-link" href="#services">Services</a>
+        </li>    
+        <li className="nav-item ">
+          <a className="nav-link "  href="#contact">Contact Us</a>
+        </li>   
+        <li className="nav-item ">
+        <a href="https://app.gigleji.com/" className="btn btn-primary btn-sm" role="button">Log in</a>
+        <span style={{ marginRight: '10px' }}></span>
+    <a href="https://app.gigleji.com/register" className="btn btn-success btn-sm" role="button">Signup</a>
+
+</li>  
+</ul>
+</div>
+</nav>
+
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
